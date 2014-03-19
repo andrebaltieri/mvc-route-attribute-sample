f7bd1c15bf9d1093983d283aba0cd8ecb0b8db65
Andre Baltieri <andrebaltieri@hotmail.com>
2014-03-19 13:44:31 -0300

    Versão inicial

---
+++ src/MvcAttributeRouting/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAttributeRouting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("sobre")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("categorias/{categoria}/produtos/{produto}")]
+        public string RouteTest(string categoria, string produto)
+        {
+            return String.Format("Listando o produto {0} da categoria {1}", produto, categoria);
+        }
+    }
+}
